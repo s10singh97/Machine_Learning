ac2c34f9e8c74d8ca830aded8ae40d9db994706c
s10singh97 <s10singh97@yahoo.in>
2018-07-22 19:05:17 +0530

    Missing Data

--- Machine Learning A-Z/Part 1 - Data Preprocessing/my_data_preprocessing_template.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/my_data_preprocessing_template.py
@@ -5,3 +5,8 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
