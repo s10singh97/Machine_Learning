8df735fd2bc5f6d9ac83faf067a11920137a4bbf
s10singh97 <s10singh97@yahoo.in>
2018-07-23 19:17:50 +0530

    Simple Regression Model

---
+++ Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/my_simple_regression.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, 0].values
+y = dataset.iloc[:, 1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+'''from sklearn.preprocessing import MinMaxScaler
+sc_X = MinMaxScaler(feature_range = (0,1))
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = MinMaxScaler(feature_range = (0, 1))
+y_train = sc_y.fit_transform(y_train)
+y_test = sc_y.transform(y_test)'''
+
