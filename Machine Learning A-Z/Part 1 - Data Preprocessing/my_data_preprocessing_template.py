16d922309b9a46e7565c7be141e7735abb2a7494
s10singh97 <s10singh97@yahoo.in>
2018-07-23 17:17:47 +0530

    Standard Scaler

--- Machine Learning A-Z/Part 1 - Data Preprocessing/my_data_preprocessing_template.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/my_data_preprocessing_template.py
@@ -21,3 +21,8 @@ y = labelencoder_y.fit_transform(y)
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 7)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
