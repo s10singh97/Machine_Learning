79bb9fa5c3f31367b2279c9615eb6e7836ca3b6c
s10singh97 <s10singh97@yahoo.in>
2018-07-24 21:11:10 +0530

    categorical data

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/my_multiple_linear_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/my_multiple_linear_regression.py
@@ -3,3 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('50_Startups.csv')
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder = LabelEncoder()
+X[:, 3] = labelencoder.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
