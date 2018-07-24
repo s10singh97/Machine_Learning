c7139fc4be4fc362354e0e62a238583a88b54219
s10singh97 <s10singh97@yahoo.in>
2018-07-24 21:42:38 +0530

    Fitting on training data

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/my_multiple_linear_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/my_multiple_linear_regression.py
@@ -17,3 +17,6 @@ X = X[:, 1:]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
