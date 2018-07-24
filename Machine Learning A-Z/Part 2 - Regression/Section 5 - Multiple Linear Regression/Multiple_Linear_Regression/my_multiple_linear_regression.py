3ed156e9271c37d419f8f383ea4264795780c16b
s10singh97 <s10singh97@yahoo.in>
2018-07-24 21:57:56 +0530

    Predicting test set

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/my_multiple_linear_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/my_multiple_linear_regression.py
@@ -20,3 +20,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
