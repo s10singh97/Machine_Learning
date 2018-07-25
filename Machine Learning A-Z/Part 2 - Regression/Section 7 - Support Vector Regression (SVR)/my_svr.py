fca228b0984c8504e25c34e936ebccb30971f391
s10singh97 <s10singh97@yahoo.in>
2018-07-25 21:49:16 +0530

    svr

---
+++ Machine Learning A-Z/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/my_svr.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, -1].values
+
