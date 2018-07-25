ea7f9caebe2b6c832dd2f381e3be382078d13b55
s10singh97 <s10singh97@yahoo.in>
2018-07-25 19:40:25 +0530

    Checking with Polynomial Regression

--- Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.py
@@ -10,4 +10,9 @@ from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 
-from sklearn
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
