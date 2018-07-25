2632a3a49eae530105bc43be9213c09f8073e749
s10singh97 <s10singh97@yahoo.in>
2018-07-25 19:32:54 +0530

    Checking with Linear Model

--- Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/my_polynomial_regression.py
@@ -5,3 +5,9 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, -1].values
+
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+from sklearn
