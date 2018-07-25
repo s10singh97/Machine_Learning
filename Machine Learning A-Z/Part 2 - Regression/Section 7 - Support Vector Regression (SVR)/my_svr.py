7ad7206ea6d4c1ddc17541fb38dacc1f9b144986
s10singh97 <s10singh97@yahoo.in>
2018-07-25 22:03:04 +0530

    svr implemented

--- Machine Learning A-Z/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/my_svr.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/my_svr.py
@@ -6,3 +6,22 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, -1].values
 
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+
+y_pred = regressor.predict(6.5)
+
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff(SVM)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.savefig('tob_svm.png')
+plt.show()
