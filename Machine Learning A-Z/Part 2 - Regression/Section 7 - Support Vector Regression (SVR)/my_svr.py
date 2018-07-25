05ff95bb2dd46a6a009043b7bfa47ad2c9e8dddf
s10singh97 <s10singh97@yahoo.in>
2018-07-25 22:13:41 +0530

    predicting using svr

--- Machine Learning A-Z/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/my_svr.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/my_svr.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, -1].values
+y = dataset.iloc[:, 2:3].values
 
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
@@ -16,7 +16,9 @@ from sklearn.svm import SVR
 regressor = SVR(kernel = 'rbf')
 regressor.fit(X, y)
 
-y_pred = regressor.predict(6.5)
+inp = sc_X.transform(np.array([[6.5]]))
+y_pred = regressor.predict(inp)
+y_pred = sc_y.inverse_transform(y_pred)
 
 plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
