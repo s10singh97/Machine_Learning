dbb3147224bac8e3ea7ae4d6203d760fe9cc739c
s10singh97 <s10singh97@yahoo.in>
2018-07-26 10:10:48 +0530

    Predicting

--- Machine Learning A-Z/Part 2 - Regression/Section 8 - Decision Tree Regression/my_decision_tree_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 8 - Decision Tree Regression/my_decision_tree_regression.py
@@ -6,3 +6,16 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
+
+y_pred = regressor.predict(6.5)
+
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff(Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.savefig('tob_dtr.png')
+plt.show()
