3abd788d03c72241d0b98a64f863a89280d50d80
s10singh97 <s10singh97@yahoo.in>
2018-07-29 11:43:12 +0530

    Confusion matrix

--- Machine Learning A-Z/Part 3 - Classification/Section 14 - Logistic Regression/my_logistic_regression.py
+++ Machine Learning A-Z/Part 3 - Classification/Section 14 - Logistic Regression/my_logistic_regression.py
@@ -19,3 +19,6 @@ classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
 y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
