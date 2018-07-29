ee507b3a79a7c9a8d92341680fbb83d39af8ef41
s10singh97 <s10singh97@yahoo.in>
2018-07-29 11:27:24 +0530

    predicting using logistic regression

--- Machine Learning A-Z/Part 3 - Classification/Section 14 - Logistic Regression/my_logistic_regression.py
+++ Machine Learning A-Z/Part 3 - Classification/Section 14 - Logistic Regression/my_logistic_regression.py
@@ -14,3 +14,8 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
