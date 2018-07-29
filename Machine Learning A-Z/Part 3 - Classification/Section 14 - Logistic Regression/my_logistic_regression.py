97f37d97963996a7c38f8feeb9f43824bce771a2
s10singh97 <s10singh97@yahoo.in>
2018-07-29 11:25:04 +0530

    data preprocessing

---
+++ Machine Learning A-Z/Part 3 - Classification/Section 14 - Logistic Regression/my_logistic_regression.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, 2:4].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
