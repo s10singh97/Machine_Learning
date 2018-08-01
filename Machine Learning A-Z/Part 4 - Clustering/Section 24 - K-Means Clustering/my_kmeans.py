bc04bb6e364f68bceb679df85079f0da94677faa
s10singh97 <s10singh97@yahoo.in>
2018-08-01 16:18:32 +0530

    Applying KMeans

--- Machine Learning A-Z/Part 4 - Clustering/Section 24 - K-Means Clustering/my_kmeans.py
+++ Machine Learning A-Z/Part 4 - Clustering/Section 24 - K-Means Clustering/my_kmeans.py
@@ -3,4 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('Mall_Customers.csv')
-X = dataset.iloc[:, ].values
+X = dataset.iloc[:, [3, 4]].values
+
+# Using elbow method to find optimal number of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.savefig('KMeans_Elbow.png')
+plt.show()
+
+# Applying KMeans
+kmeans = KMeans(n_clusters = 5, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(X)
