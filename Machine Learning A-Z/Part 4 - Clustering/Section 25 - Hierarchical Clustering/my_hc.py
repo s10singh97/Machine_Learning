a600e9d574c17f9a7a5d1fd66d309293f712a4af
s10singh97 <s10singh97@yahoo.in>
2018-08-03 10:46:28 +0530

    Dendogram

--- Machine Learning A-Z/Part 4 - Clustering/Section 25 - Hierarchical Clustering/my_hc.py
+++ Machine Learning A-Z/Part 4 - Clustering/Section 25 - Hierarchical Clustering/my_hc.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
+
+import scipy.cluster.hierarchy as sch
+dendogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title('Dendogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean Distance')
+plt.savefig('HC_dendogram.png')
+plt.show()
