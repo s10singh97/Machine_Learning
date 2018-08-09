2626a7c4289eb07256a4b898b26bc038a5ae56ce
s10singh97 <s10singh97@yahoo.in>
2018-08-09 21:01:05 +0530

    data preprocessing for eclat

---
+++ Machine Learning A-Z/Part 5 - Association Rule Learning/Section 29 - Eclat/my_eclat.py
@@ -0,0 +1,11 @@
+# Eclat
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+    
