46caa50801a3efe2fa0573fdcdb4a8c0f4bc6b82
s10singh97 <s10singh97@yahoo.in>
2018-08-12 13:18:21 +0530

    ucb(initial)

--- Machine Learning A-Z/Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/my_upper_confidence_bound.py
+++ Machine Learning A-Z/Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/my_upper_confidence_bound.py
@@ -3,3 +3,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+import math
+N = 10000
+d = 10
+ads_selected = []
+numbers_of_selections = [0]*d
+sums_of_reward = [0]*d
+for n in range(0, N):
+    ad = 0
+    max_upper_bound = 0
+    for i in range(0, d):
+        if (numbers_of_selections[i] > 0):
+            average_reward = sums_of_reward[i] / numbers_of_selections[i]
+            delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
+            upper_bound = average_reward + delta_i
+        else:
+            upper_bound = 1e400
+        if upper_bound > max_upper_bound:
+            max_upper_bound = upper_bound
+            ad = i
