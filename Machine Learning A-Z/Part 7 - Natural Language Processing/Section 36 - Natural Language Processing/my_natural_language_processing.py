d9121a90eb3522f2ca404205eec5e1ad0cb97243
s10singh97 <s10singh97@yahoo.in>
2018-08-15 11:17:42 +0530

    Cleaning of texts

--- Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/my_natural_language_processing.py
+++ Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/my_natural_language_processing.py
@@ -3,3 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning of texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
