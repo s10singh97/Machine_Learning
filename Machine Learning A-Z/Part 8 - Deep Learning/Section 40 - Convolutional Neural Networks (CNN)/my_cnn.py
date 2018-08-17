decf7de8ef122c6869c479ee5533faf60f35124e
s10singh97 <s10singh97@yahoo.in>
2018-08-17 22:58:58 +0530

    Buiding the model

--- Machine Learning A-Z/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/my_cnn.py
+++ Machine Learning A-Z/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/my_cnn.py
@@ -1,2 +1,17 @@
-# -*- coding: utf-8 -*-
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+
+classifier = Sequential()
+classifier.add(Convolution2D(32, 3, 3, activation = 'relu', input_shape = (64, 64, 3)))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Flatten())
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+
+classifier.compile(optimizer = 'adam', metrics = ['accuracy'], loss = 'binary_crossentropy')
 
