6efb93f0d6e1a0ea329a236bfa4302280a62e15e
s10singh97 <s10singh97@yahoo.in>
2018-08-17 23:17:15 +0530

    Final Mods for CNN

--- Machine Learning A-Z/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/my_cnn.py
+++ Machine Learning A-Z/Part 8 - Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/my_cnn.py
@@ -5,13 +5,13 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 
 classifier = Sequential()
-classifier.add(Convolution2D(32, 3, 3, activation = 'relu', input_shape = (64, 64, 3)))
+classifier.add(Convolution2D(32, (3, 3), activation = 'relu', input_shape = (64, 64, 3)))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
-classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+classifier.add(Convolution2D(32, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 classifier.add(Flatten())
-classifier.add(Dense(output_dim = 128, activation = 'relu'))
-classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 classifier.compile(optimizer = 'adam', metrics = ['accuracy'], loss = 'binary_crossentropy')
 
@@ -36,8 +36,14 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             batch_size = 32,
                                             class_mode = 'binary')
 
-classifier.fit_generator(training_set,
-                         samples_per_epoch = 8000,
-                         nb_epoch = 25,
-                         validation_data = test_set,
-                         nb_val_samples = 2000)
+#classifier.fit_generator(training_set,
+#                         samples_per_epoch = 8000,
+#                         nb_epoch = 25,
+#                         validation_data = test_set,
+#                         nb_val_samples = 2000)
+classifier.fit_generator(
+        training_set,
+        steps_per_epoch=8000,
+        epochs=25,
+        validation_data=test_set,
+        validation_steps=2000)
