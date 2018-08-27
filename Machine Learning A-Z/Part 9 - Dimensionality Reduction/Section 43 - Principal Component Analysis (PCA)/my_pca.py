dfa1c95d1a03f2aea31cdf925eb2b4cda6e196ad
s10singh97 <s10singh97@yahoo.in>
2018-08-27 18:29:48 +0530

    data preprocessing

--- Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/my_pca.py
+++ Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/my_pca.py
@@ -1,2 +1,16 @@
-# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Wine.csv')
+X = dataset.iloc[:, 0:13].values
+y = dataset.iloc[:, 13].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_teat = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_train)
 
