b2a7ba10aa3130e0e161adf7167747ca4ac38966
s10singh97 <s10singh97@yahoo.in>
2018-08-27 18:31:26 +0530

    fitting Logistic Regression

--- Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/my_pca.py
+++ Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/my_pca.py
@@ -14,3 +14,7 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_train)
 
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
