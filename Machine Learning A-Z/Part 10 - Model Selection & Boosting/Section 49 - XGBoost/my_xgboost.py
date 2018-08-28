d8f49568c46427f91a84d85bc7f9580387789b1a
s10singh97 <s10singh97@yahoo.in>
2018-08-28 10:44:08 +0530

    fitting XGBoost

--- Machine Learning A-Z/Part 10 - Model Selection & Boosting/Section 49 - XGBoost/my_xgboost.py
+++ Machine Learning A-Z/Part 10 - Model Selection & Boosting/Section 49 - XGBoost/my_xgboost.py
@@ -19,3 +19,16 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Fitting XGBoost to the Training set
+from xgboost import XGBClassifier
+classifier = XGBClassifier()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies.mean()
+accuracies.std()
